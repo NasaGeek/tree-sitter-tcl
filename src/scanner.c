47cfebad59587e71bae1495cb0ac317e633bb972
Chris Roberts <chris.roberts@flightaware.com>
2024-08-26 02:09:26 -0500

    Include debug logging in scanner
    Gotta turn on/off so it doesn't spam the test output. Should use the
    builtin lexer->log() once it's released.

--- src/scanner.c
+++ src/scanner.c
@@ -1,5 +1,9 @@
 #include "tree_sitter/parser.h"
 #include <wctype.h>
+#ifndef __wasm__
+#include <stdarg.h>
+#include <stdio.h>
+#endif
 
 enum TokenType {
   CONCAT,
@@ -9,6 +13,24 @@ enum TokenType {
   // scanner is called with all symbols marked as valid.
   ERROR,
 };
+
+#define DEBUG 0
+
+static bool myprintf(const char *format, ...) {
+#if DEBUG
+#ifndef __wasm__
+    va_list args;
+    int result;
+    va_start(args, format);
+    result = vprintf(format, args);
+    va_end(args);
+    return result;
+#else
+    return 1;
+#endif
+#endif
+    return 1;
+}
 
 static bool is_eof(TSLexer *lexer) {
   return lexer->lookahead == 0;
@@ -51,18 +73,22 @@ void *tree_sitter_tcl_external_scanner_create() {
 
 bool tree_sitter_tcl_external_scanner_scan(void *payload, TSLexer *lexer,
                                           const bool *valid_symbols) {
+  myprintf("Column: %d, lookahead: '%c', symbols: %d %d %d\n", lexer->get_column(lexer), lexer->lookahead, valid_symbols[CONCAT], valid_symbols[NS_DELIM], valid_symbols[ERROR]);
   if (valid_symbols[ERROR]) {
       return false;
   }
   if (valid_symbols[NS_DELIM] && scan_ns_delim(lexer)) {
+    myprintf("We picked ns\n");
     return true;
   }
 
   if (is_concat_valid(lexer, valid_symbols)) {
+    myprintf("We picked concat\n");
     lexer->result_symbol = CONCAT;
     return true;
   }
 
+  myprintf("We picked neither\n");
   return false;
 }
 
